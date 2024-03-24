b2b803123d9dbd760eb24e4e7a4db972d4db2972
shad <shadislam999@gmail.com>
2024-03-24 23:05:14 +0600

    notification, search bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -19,26 +19,76 @@ class _HomePageState extends State<HomePage> {
           child: Column(
           
             children: [
+              //greeting row
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    'shad',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                    
-                    
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'shad',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 10,),
+                      Text(
+                        '24 march, 2024',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue[100],
+                        ),
+                        ),
+                    ],
                   ),
                   //notification
-                  Icon(
-                    Icons.notifications,
-                    color: Colors.white,
-
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[600],
+                      borderRadius: BorderRadius.circular(12),
+                     
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Icon(
+                        Icons.notifications,
+                        color: Colors.white,
+                                      
+                      ),
+                    ),
                   ),
                 ],
               ),
+              SizedBox(height: 20,),
+              // search bar
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue[600],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    children: [
+                     
+                      Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        ),
+                        Text(
+                          'Search',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                          ),
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
